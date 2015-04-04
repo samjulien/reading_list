c76ae3280262ca0b166c30ef90fdc72e853caf27
samjulien <spjulien@gmail.com>
2015-04-03 18:41:50 -0700

    added keyword search and a few more styles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150403144753) do
+ActiveRecord::Schema.define(version: 20150404010551) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150403144753) do
     t.datetime "updated_at",  null: false
     t.integer  "rating"
     t.date     "finished_on"
+    t.text     "keywords"
   end
 
 end
